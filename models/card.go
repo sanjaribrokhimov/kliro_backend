fbc120054f60616e856d9bf494d506cf287d827f
sanjaribrokhimov <sanjaribrokhimov3210@mail.ru>
2025-08-03 23:32:14 -0700

    lower case card rsponse

--- models/card.go
+++ models/card.go
@@ -5,12 +5,11 @@ import (
 )
 
 type Card struct {
-	ID          uint      `gorm:"primaryKey"`
-	BankName    string
-	Title       string
-	Currency    string
-	System      string
-	OpeningType string
-	CreatedAt   time.Time
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	BankName    string    `json:"bank_name"`
+	Title       string    `json:"title"`
+	Currency    string    `json:"currency"`
+	System      string    `json:"system"`
+	OpeningType string    `json:"opening_type"`
+	CreatedAt   time.Time `json:"created_at"`
 }
-
