effa2ad554c6e6b70adaae3fed09a6be78899607
sanjaribrokhimov <sanjaribrokhimov3210@mail.ru>
2025-09-12 07:28:06 -0700

    fix .env

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 func main() {
 
 	// Загрузка .env
-	err := godotenv.Load("./.env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Println("No .env file found, using environment variables")
 	}
